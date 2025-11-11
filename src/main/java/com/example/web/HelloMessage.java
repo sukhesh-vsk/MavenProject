c119dbb6cadaae8fab9169db0bda725658b6dfca
sukhesh-vsk <sukhesh.vsk2005@gmail.com>
2025-11-11 08:27:24 +0530

    update test

---
+++ src/main/java/com/example/web/HelloMessage.java
@@ -0,0 +1,7 @@
+package com.example.web;
+
+public class HelloMessage {
+    static String getHelloMessage() {
+        return "Hello, World !!";
+    }
+}
---
+++ src/main/java/com/example/web/HelloMessageTest.java
@@ -0,0 +1,14 @@
+package com.example.web;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class HelloMessageTest {
+    @Test
+    public void testGetHelloMessage() {
+        String expected = "HelloWorld";
+        String actual = "HelloWorld";
+
+        assertEquals(expected, actual);
+    }
+}
